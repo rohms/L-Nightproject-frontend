528141b2aa57ca600a963a53990dd55837ab45ed
Roosa H <[sirkusraja@gmail.com]>
2021-10-18 19:04:04 +0200

    added the gallery pics and the gallery page

--- src/Components/Gallery.js
+++ src/Components/Gallery.js
@@ -1,11 +1,17 @@
 import React from 'react'
-import { ImageList, ImageListItem } from "@mui/material"; 
+import { ImageList, ImageListItem } from "@mui/material";
+import GalleryPics from './Gallerypics';
+import "../Components/Styles/Gallery.css"
+
 
 const Gallery = () => {
     return (
         <div>
             <h2>Gallery</h2>
-          
+                <div className="Gallerypicscontainer">
+                    <ImageList variant="masonry" cols={4} gap={8}> {GalleryPics.map((item, index) => ( <ImageListItem key={index}> <img src={`${item.imgSrc}?w=248&fit=crop&auto=format`} srcSet={`${item.imgSrc}?w=248&fit=crop&auto=format&dpr=2 2x`} alt={item.title} loading="lazy" />
+                    </ImageListItem> ))} </ImageList> 
+            </div>
         </div>
     )
 }
