798606549f866b35ba07a95a3747067c549cf56e
Roosa H <[sirkusraja@gmail.com]>
2022-01-09 20:16:13 +0100

    cleaned more code

--- src/AppRouter.js
+++ src/AppRouter.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import About from "./Components/About";
 import CalendarMain from "./Components/CalendarMain";
 import Contactpage from "./Components/Contactpage";
