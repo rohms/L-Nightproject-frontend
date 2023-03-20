8f1f3f031493f1c5c5c65a8aa114f8ef9f11b5c5
Roosa H <[sirkusraja@gmail.com]>
2023-03-20 23:10:09 +0100

    reinstalled react toastify

--- src/Components/Alerts/Toast.js
+++ src/Components/Alerts/Toast.js
@@ -1,26 +1,24 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-
-
+import React from "react";
+import PropTypes from "prop-types";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const Toast = ({ msg }) => {
-  const notify = () => toast("Wow")
   return (
     <ToastContainer
-    position="top-center"
-    autoClose={5000}
-    hideProgressBar={true}
-    newestOnTop={false}
-    closeOnClick={true}
-    rtl={false}
-    pauseOnFocusLoss
-    draggable={true}
-    pauseOnHover={true}
-    theme="colored" />
-  )
-}
+      position="top-center"
+      autoClose={5000}
+      hideProgressBar={true}
+      newestOnTop={false}
+      closeOnClick={true}
+      rtl={false}
+      pauseOnFocusLoss
+      draggable={true}
+      pauseOnHover={true}
+      theme="colored"
+    />
+  );
+};
 
 // Toast.propTypes = {
 //   msg: PropTypes.string.isRequired
