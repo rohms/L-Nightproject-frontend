e7a059b2f0fe9525f145f38c88547460f1cd00d1
Roosa H <[sirkusraja@gmail.com]>
2023-07-12 14:26:16 +0200

    restyled 404 page

--- src/Components/Alerts/NotFound.js
+++ src/Components/Alerts/NotFound.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import Gradient from 'rgt'
 
 const NotFound = () => {
     return (
         <div className="not-found">
-            <h1>404</h1>
-            <h2>Page not found :(</h2>
+            <Gradient dir="left-to-right" from="#e30cad" to="#ff8000"><h1>404</h1></Gradient>
+            <h2>This page was not found! Unicorns cry!</h2>
+            <img src="https://media.tenor.com/0jI-YXeywSsAAAAC/nyan-cat-cat.gif" width="390" height="390" alt="Nyan Cat Cat GIF - Nyan Cat Cat Kawaii Cat GIFs" />
         </div>
     )
 }
