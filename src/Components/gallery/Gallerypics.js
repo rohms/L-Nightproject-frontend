b07b629788427d3b84d448ae3da5d6ca9f5f0216
Roosa <86847314+rohms@users.noreply.github.com>
2024-02-05 22:53:17 +0100

    Revert "fixed image upload"

---
+++ src/Components/gallery/Gallerypics.js
@@ -0,0 +1,95 @@
+import Copper from "../../Images/Gallerypics/Copper.jpg";
+import Coronet from "../../Images/Gallerypics/Coronet.jpg";
+import Flowers from "../../Images/Gallerypics/Flowers.jpg";
+import Lake from "../../Images/Gallerypics/Lake.jpg";
+import Lighthouse from "../../Images/Gallerypics/Lighthouse.jpg";
+import MonasteryGarden from "../../Images/Gallerypics/MonasteryGarden.jpg";
+import Mushroom from "../../Images/Gallerypics/Mushroom.jpg";
+import Sticks from "../../Images/Gallerypics/PickUpSticks.jpg";
+import Drops from "../../Images/Gallerypics/Raindrops.jpg";
+import Seed from "../../Images/Gallerypics/Seed.jpg";
+import Web from "../../Images/Gallerypics/SpiderWeb.jpg";
+import Motion from "../../Images/Gallerypics/StopMotion.jpg";
+import WaterDrops from "../../Images/Gallerypics/WaterDrops.jpg";
+import Waterfall from "../../Images/Gallerypics/Waterfall.jpg";
+import lnightbb from "../../Images/Gallerypics/lnightbb.jpg";
+
+const GalleryPics = [
+  {
+    id: 1,
+    src: lnightbb,
+    title: "L-Night at Dykemarch 2021",
+  },
+  {
+    id: 2,
+    src: Copper,
+    title: "Copper coloured leaves",
+  },
+  {
+    id: 3,
+    src: Coronet,
+    title: "Picture of a Coronet",
+  },
+  {
+    id: 4,
+    src: Flowers,
+    title: "Flowers",
+  },
+  {
+    id: 5,
+    src: Lake,
+    title: "A lake",
+  },
+  {
+    id: 6,
+    src: Lighthouse,
+    title: "Lighthouse",
+  },
+  {
+    id: 7,
+    src: MonasteryGarden,
+    title: "Monastery Garden",
+  },
+  {
+    id: 8,
+    src: Mushroom,
+    title: "Mushrooms growing",
+  },
+  {
+    id: 9,
+    src: Sticks,
+    title: "Sticks",
+  },
+  {
+    id: 10,
+    src: Drops,
+    title: "Waterdrops",
+  },
+  {
+    id: 11,
+    src: Seed,
+    title: "Seed",
+  },
+  {
+    id: 12,
+    src: Web,
+    title: "Spiderweb",
+  },
+  {
+    id: 13,
+    src: Motion,
+    title: "Stopmotion",
+  },
+  {
+    id: 14,
+    src: WaterDrops,
+    title: "Waterdrops",
+  },
+  {
+    id: 15,
+    src: Waterfall,
+    title: "Waterfall",
+  },
+];
+
+export default GalleryPics;
